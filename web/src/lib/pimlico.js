5bdd5ae8edb148a8649dc7d431bc251cebc7681e
Dan Miles <dan@kilometre.dev>
2024-03-16 06:41:51 +0000

    generalise

--- web/src/lib/pimlico.js
+++ web/src/lib/pimlico.js
@@ -1,7 +1,8 @@
 import {createPublicClient, http} from "viem";
 import {baseSepolia} from "viem/chains";
 import {createPimlicoBundlerClient, createPimlicoPaymasterClient} from "permissionless/clients/pimlico";
-import {ENTRYPOINT_ADDRESS_V06} from "permissionless";
+import {createSmartAccountClient, ENTRYPOINT_ADDRESS_V06, walletClientToSmartAccountSigner} from "permissionless";
+import {signerToSafeSmartAccount} from "permissionless/accounts";
 
 export const pimlicoRPC = "https://api.pimlico.io/v2/base-sepolia/rpc?apikey=cb041e12-7980-4c33-9d3f-f8e0fd3172b7"
 
@@ -19,3 +20,42 @@ export const pimlicoBundlerClient = createPimlicoBundlerClient({
 	transport: http(pimlicoRPC),
 	entryPoint: ENTRYPOINT_ADDRESS_V06,
 })
+
+export const doClientSetup = async (data, signer, safeAccount, smartAccountClient) => {
+	if (!data) {
+		throw new Error("No wallet data")
+	}
+	signer.current = walletClientToSmartAccountSigner(data)
+	safeAccount.current = await signerToSafeSmartAccount(publicClient, {
+		entryPoint: ENTRYPOINT_ADDRESS_V06,
+		signer: signer.current,
+		safeVersion: "1.4.1",
+	})
+	smartAccountClient.current = createSmartAccountClient({
+		account: safeAccount.current,
+		chain: baseSepolia, // or whatever chain you are using
+		bundlerTransport: http(pimlicoRPC),
+		entryPoint: ENTRYPOINT_ADDRESS_V06,
+		middleware: {
+			gasPrice: async () => (await pimlicoBundlerClient.getUserOperationGasPrice()).fast, // use pimlico bundler to get gas prices
+			sponsorUserOperation: paymasterClient.sponsorUserOperation, // optional
+		},
+	})
+
+}
+
+
+export const safeSendTxn = async (txn, data, signer, safeAccount, smartAccountClient) => {
+	const gasPrices = await pimlicoBundlerClient.getUserOperationGasPrice()
+	console.log("Gas prices", gasPrices)
+	txn.maxFeePerGas = gasPrices.fast.maxFeePerGas
+	txn.maxPriorityFeePerGas = gasPrices.fast.maxPriorityFeePerGas
+	try {
+		await doClientSetup(data, signer, safeAccount, smartAccountClient)
+		console.log(smartAccountClient)
+		const txHash = await smartAccountClient.current.sendTransaction(txn)
+		console.log(txHash)
+	} catch (e) {
+		console.error(e)
+	}
+}
--- web/src/pages/login.js
+++ web/src/pages/login.js
@@ -1,11 +1,8 @@
 import {DynamicWidget} from "@dynamic-labs/sdk-react-core";
-import {createSmartAccountClient, ENTRYPOINT_ADDRESS_V06, walletClientToSmartAccountSigner,} from "permissionless";
-import {signerToSafeSmartAccount} from "permissionless/accounts";
 import {useWalletClient} from "wagmi";
-import {baseSepolia} from "viem/chains";
-import {http, parseEther, zeroAddress} from "viem";
-import {paymasterClient, pimlicoBundlerClient, pimlicoRPC, publicClient} from "@/lib/pimlico";
-import {useEffect, useRef} from "react";
+import {parseEther, zeroAddress} from "viem";
+import {doClientSetup, safeSendTxn} from "@/lib/pimlico";
+import {useRef} from "react";
 
 export default function Login() {
 
@@ -15,33 +12,10 @@ export default function Login() {
 	const smartAccountClient = useRef({})
 
 
-	const doClientSetup = async () => {
-		if (!data) {
-			throw new Error("No wallet data")
-		}
-		signer.current = walletClientToSmartAccountSigner(data)
-		safeAccount.current = await signerToSafeSmartAccount(publicClient, {
-			entryPoint: ENTRYPOINT_ADDRESS_V06,
-			signer: signer.current,
-			safeVersion: "1.4.1",
-		})
-		smartAccountClient.current = createSmartAccountClient({
-			account: safeAccount.current,
-			chain: baseSepolia, // or whatever chain you are using
-			bundlerTransport: http(pimlicoRPC),
-			entryPoint: ENTRYPOINT_ADDRESS_V06,
-			middleware: {
-				gasPrice: async () => (await pimlicoBundlerClient.getUserOperationGasPrice()).fast, // use pimlico bundler to get gas prices
-				sponsorUserOperation: paymasterClient.sponsorUserOperation, // optional
-			},
-		})
-
-	}
-
 	const createSmartAccount = async () => {
 		console.log("data", data)
 		try {
-			await doClientSetup()
+			await doClientSetup(data, signer, safeAccount, smartAccountClient)
 			// unsure if this does what we want it to.
 			const txHash = await smartAccountClient.current.sendTransaction({
 				to: zeroAddress,
@@ -63,28 +37,13 @@ export default function Login() {
 		})
 	}
 
-	const safeSendTxn = async (txn) => {
-		const gasPrices = await pimlicoBundlerClient.getUserOperationGasPrice()
-		console.log("Gas prices", gasPrices)
-		txn.maxFeePerGas = gasPrices.fast.maxFeePerGas
-		txn.maxPriorityFeePerGas = gasPrices.fast.maxPriorityFeePerGas
-		try {
-			await doClientSetup()
-			console.log(smartAccountClient)
-			const txHash = await smartAccountClient.current.sendTransaction(txn)
-			console.log(txHash)
-		} catch (e) {
-			console.error(e)
-		}
-	}
-
 	const sendSafeEthHandler = (e) => {
 		e.preventDefault()
 		const demoSendEth = {
 			to: "0xaf785f9296741a3BAF34eA2A6b576ACAFA30B6Ec", // 3266miles.eth
 			value: parseEther("0.001")
 		}
-		safeSendTxn(demoSendEth).then(() => {
+		safeSendTxn(demoSendEth, data, signer, safeAccount, smartAccountClient).then(() => {
 			console.log("SAFE ETH sent")
 		}).catch((e) => {
 			console.error(e)
